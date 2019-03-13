3b4b53dc04b466aea28e817d596c1f154be53d3d
xiaok <xiaokang136106@163.com>
2019-03-13 12:56:35 +0800

    first commit

---
+++ Desktop/letao-master/letao-cart/letao-cart-interface/src/main/java/com/xiaok/cart/service/CartService.java
@@ -0,0 +1,23 @@
+package com.xiaok.cart.service;
+
+
+
+import java.util.List;
+
+/**
+ * <pre>
+ * Description:
+ * Copyright:	Copyright (c)2017
+ * Author:		lllx
+ * Version:		1.0
+ * Created at:	2018/1/4
+ * </pre>
+ */
+public interface CartService {
+    TaotaoResult addCart(long userId, long itemId, int num);
+    TaotaoResult mergeCart(long userId, List<TbItem> itemList);
+    List<TbItem> getCartList(long userId);
+    TaotaoResult updateCartNum(long userId, long itemId, int num);
+    TaotaoResult deleteCartItem(long userId, long itemId);
+    TaotaoResult clearCartItem(long userId);
+}
